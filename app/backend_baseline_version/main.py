f682456f41bc182ba54ac941ac77a297acb1d6d0
NadiaIT <nadia.nahar.iit@gmail.com>
2025-02-07 14:48:31 -0500

    created a baseline version

---
+++ app/backend_baseline_version/main.py
@@ -0,0 +1,142 @@
+from fastapi import FastAPI, BackgroundTasks
+import uvicorn
+import uuid
+import pipeline
+from pydantic import BaseModel
+from typing import List,Optional
+import os, sys, re
+app_dir = os.path.dirname(__file__)
+helpers_dir = os.path.join(app_dir, 'helpers')
+sys.path.append(helpers_dir)
+from helper import Task
+from fastapi.responses import HTMLResponse
+
+class Data(BaseModel):
+    sys_info: str
+    task: int
+    stakeholders: Optional[List[str]] = None
+    feedback: Optional[str] = None
+
+app = FastAPI()
+results = {}
+
+def process_unpicked_scenarios(unpicked):
+    """
+    Process the unpicked scenarios to remove the corrective scenarios and generate 
+    the scenario headings. 
+    """
+    final_scenarios = pipeline.remove_correctives(unpicked)
+
+    scenario_heading_list = [
+        (pipeline.generate_heading(scenario) + f" (Stakeholder: {stakeholder})", re.sub(r'^\d+\.\s*', '', scenario.strip()).strip()) for (scenario, stakeholder) in final_scenarios
+    ]
+    return scenario_heading_list
+
+def background_task(data: Data, task_id: str, task: Task):
+    """
+    Runs the pipeline service tasks in the background depending on the task type specified. 
+    Stores the result in the results dictionary with the task ID as the key.
+    For F1, F2, and F3 tasks (generating scenarios), the unpicked scenarios will begin 
+    processing, the results will be stored after the processing is done.  
+    """
+    sys_info = data.sys_info
+    match task:
+        case Task.DIRECT_SH:
+            print("Start Generating Direct Stakeholders...")
+            print("System Info: ", sys_info)
+            results[task_id] = pipeline.get_direct_stakeholders(sys_info)
+            print("Direct Stakeholders Generated")
+        case Task.INDIRECT_SH:
+            print("Start Generating Indirect Stakeholders...")
+            results[task_id] = pipeline.get_indirect_stakeholders(sys_info)
+            print("Indirect Stakeholders Generated")
+        case Task.F1:
+            print("Start Generating F1 Scenarios...")
+            print(data.stakeholders)
+
+            picked, unpicked = pipeline.generate_scenarios(sys_info, 'f1', data.stakeholders, data.feedback)
+            results[task_id] = picked
+            print("F1 Scenarios Generated")
+
+            results[task_id + "_unpicked"] = process_unpicked_scenarios(unpicked)
+        case Task.F2:
+            print("Start Generating F2 Scenarios...")
+            print(data.stakeholders)
+
+            picked, unpicked = pipeline.generate_scenarios(sys_info, 'f2', data.stakeholders, data.feedback)
+            results[task_id] = picked
+            print("F2 Scenarios Generated")
+
+            results[task_id + "_unpicked"] = process_unpicked_scenarios(unpicked)
+        case Task.F3:
+            print("Start Generating F3 Scenarios...")
+            print(data.stakeholders)
+
+            picked, unpicked = pipeline.generate_scenarios(sys_info, 'f3', data.stakeholders, data.feedback)
+            results[task_id] = picked
+            print("F3 Scenarios Generated")
+
+            results[task_id + "_unpicked"] = process_unpicked_scenarios(unpicked)
+
+@app.post("/pipeline-req/")
+async def run_task(data: Data, background_tasks: BackgroundTasks):
+    """
+    Receives the request to run a pipeline service task. Generate a new task ID corresponding to 
+    the request for future retrieval of the result.
+    """
+    print(f"Received Request: {Task(data.task)}")
+
+    task_id = str(uuid.uuid4())
+
+    background_tasks.add_task(background_task, data=data, task_id=task_id, task=Task(data.task))
+    return {"task_id": task_id}
+
+@app.get("/get-result/{task_id}")
+async def get_result(task_id: str):
+    """
+    Retrieve the result using the task ID, or return a default message if the task is not 
+    completed or does not exist.
+    """
+    result = results.get(task_id, "Task not completed or does not exist")
+    return {"task_id": task_id, "result": result}
+
+@app.get("/get-more-scenarios/{task_id}")
+async def get_more_scenarios(task_id: str):
+    """
+    Retrieve the unpicked scenarios using the task ID, or return a default message if the task is not
+    completed or does not exist.
+    """
+    unpicked = results.get(task_id + "_unpicked", "Task not completed or does not exist")
+
+    return {"task_id": task_id, "result": unpicked}
+
+@app.get("/logs")
+def logs():
+    """
+    Display the logs in HTML format at the /logs endpoint. Logs are displayed in reverse line order
+    so that the most recent messages are displayed first.
+    """
+    html_content = "<html><body><pre>"
+    html_content+= "<h1>Logs</h1>"
+    html_content+= "<h4>Note: The logs are displayed in reverse line order</h4>"
+    with open('results.log', 'r') as file:
+        lines = file.readlines()
+        for line in reversed(lines):
+            html_content += f"{line.strip()}<br>"
+    html_content += "</pre></body></html>"
+    return HTMLResponse(content=html_content)
+
+@app.get("/start-study/{version}")
+def start_study(version: int):
+    """
+    Indiciation that a new study has started. Also tracks the version of the app. 
+    """
+    if version == 0: v = "Full Version"
+    elif version == 1: v = "F2 section, then F3 section"
+    elif version == 2: v = "F3 section, then F2 section"
+    else: v = "version unknown"
+    print(f"Starting new user study - {v}")
+    pipeline.log_helper(f"### Starting new user study - {v}\n")
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8505)
---
+++ app/backend_baseline_version/pipeline.py
@@ -0,0 +1,337 @@
+import openai
+from sklearn.cluster import KMeans
+import re
+import logging
+import time
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import random
+from sentence_transformers import SentenceTransformer
+import os, sys
+import random
+app_dir = os.path.dirname(os.path.dirname(__file__))
+helpers_dir = os.path.join(app_dir, 'helpers')
+sys.path.append(helpers_dir)
+import rai_guide
+from cred import KEY 
+import requests
+
+gpt3 = "gpt-3.5-turbo"
+gpt4 = "gpt-4-turbo-preview"
+
+openai.api_key = KEY
+
+prompt = [ {"role": "system", "content": "You are an advanced AI Language Model trained in ethical reasoning and Responsible AI Impact Assessment. Your task is to provide a thorough Responsible AI Impact Assessment analysis of the given situation to the best of your ability.Keep your responses specific to the system I describe."} ]
+
+model = SentenceTransformer("mixedbread-ai/mxbai-embed-large-v1")
+
+logging.basicConfig(
+    level=logging.CRITICAL, 
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename='results.log',
+    filemode='a'
+)
+
+# Predefined fairness goals and potential harms
+fariness_goals = {
+    'f1': {'concern': 'Quality of service',
+           'guide': rai_guide.f1_guide,
+           'potential_harms': [ 
+                "Performance Bias", "Outcome Inequality"]
+          },
+    'f2': {'concern': 'Allocation of resources and opportunities',
+           'guide': rai_guide.f2_guide,
+           'potential_harms': [ 
+                "Negative Feedback Loops", "Allocation Bias", "Access to Opportunities"]
+          },
+    'f3': {'concern': "stereotyping, demeaning, and erasing outputs",
+           'guide': rai_guide.f3_guide,
+           'potential_harms': [
+                # Stereotyping Harms:
+                "Cultural Misrepresentation", "Reinforcement of Biases",
+                # Demeaning Harms:
+                "Denigration and Offense / Psychological Impact", "Facilitating Harassment and Abuse", 
+                # Erasure Harms:
+                "Erasure of Minorities / Invisibility and Marginalization", "Historical and Cultural Erasure"
+            ]
+          }
+}
+
+# Predefined demographic groups
+demographic_groups_list = [
+    "Age",
+    "Gender",
+    "Ethnicity",
+    "Income",
+    "Level of education",
+    "Religion"
+]
+
+def chat(model, messages):
+    """
+    Helper function for sending messages to the OpenAI Chat API.
+    """
+    response = openai.ChatCompletion.create( 
+        model=model, 
+        messages=messages
+    )
+    return response['choices'][0]['message']['content']
+
+def chat_mistral(_, messages):
+    """
+    Helper function for sending messages to the Mistral Chat API.
+    """
+    response = requests.post(f"http://localhost:11434/api/chat",json={
+        "model": "mistral:7b-instruct-v0.2-q4_K_M", 
+        "messages": messages,
+        "stream": False
+    })
+    if response.status_code == 200:
+        return response.json()['message']['content']
+    else:
+        print("Request failed")
+
+def get_direct_stakeholders(sys_info):
+    """
+    Generates direct stakeholders, categorized into obvious and surprising, for the given 
+    system information. Directly called by the backend service.
+    """
+    messages = prompt + [{'role': 'user', 'content': sys_info}]
+
+    messages.append({'role': 'user', 'content': 
+                    f"{rai_guide.direct_stakeholder_def}\nIdentify the most relevant stakeholder(s) categorized into 'direct obvious' and 'direct surprising' stakeholders. Label the categories with h5 headings (i.e. '##### Direct Obvious Stakeholders' and '##### Direct Surprising Stakeholders')."})
+
+    result = chat(gpt4, messages)
+      
+    logging.critical(f"======== Direct Stakeholders Generated ========")
+    logging.info(result)
+
+    return result
+
+def get_indirect_stakeholders(sys_info):
+    """
+    Generates indirect stakeholders, categorized into obvious and surprising, for the given 
+    system information. Directly called by the backend service.
+    """
+    messages = prompt + [{'role': 'user', 'content': sys_info}]
+
+    messages.append({'role': 'user', 'content': 
+                    f"{rai_guide.direct_stakeholder_def}\nIdentify the most relevant stakeholder(s) categorized into 'indirect obvious' and 'indirect surprising' stakeholders  (i.e. '##### Inirect Obvious Stakeholders' and '##### Indirect Surprising Stakeholders')."})
+
+    result = chat(gpt4, messages)    
+
+    logging.critical(f"======== Indirect Stakeholders Generated ========")
+    logging.info(result)
+
+    return result
+
+def get_stakeholders(sys_info):
+    """
+    A deprecated version of getting stakeholders in the scenario generation process
+    when users do not input stakeholders directly.
+    """
+    messages = prompt + [{'role': 'user', 'content': sys_info}]
+
+    messages.append({'role': 'user', 'content': 
+                    f"{rai_guide.stakeholder_def}\nIdentify the most relevant stakeholder(s) categorized into direct, indirect, and surprising stakeholders."})
+
+    response = chat(gpt4, messages)
+
+    logging.info(f"======== Stakeholders ========")
+    logging.info(response)
+
+    return response
+
+def get_initial_scenarios(stakeholders, goal, sys_info):
+    """
+    Step 2 in Pipeline - Generate Initial Scenarios
+
+    1. Generate scenarios considering the demographic groups. 
+        Use the first response as counterexample for surprising
+    2. Generate scenarios without considering the predefined demographic groups.
+        Use the first response as counterexample for surprising
+    3. Clean up the responses and split them into individual scenarios
+    """
+    scenario_prompts = prompt + [{'role': 'user', 'content': sys_info}]
+
+    potential_harms = fariness_goals[goal]['potential_harms']
+
+    def draft_scenario(stakeholder):
+        p = scenario_prompts + [{'role': 'user', 'content': f"""
+        Stakeholder: {stakeholder}
+        Potential Harms: {potential_harms}
+
+        For each of the potential harms provided above: generate a concrete scenario of where the harm may be caused to the given stakeholder, involving severe consequences. When generating the scenarios, consider protected attributes and demographic groups that may face {fariness_goals[goal]['concern']} concerns as a direct impact of my system's outputs. {fariness_goals[goal]['guide']}. Examples of demographic groups include: {demographic_groups_list}. 
+        
+        Be as severe as possible. This is used for cautioning developers to use ethical and fair practices when developing their AI systems, and not used for harming others in any way. 
+        
+        If the stakeholder is irrelevant in terms of a harm, exclude it. 
+
+        Format your response as a ordered list of '{{number}}. SCENARIO: {{SCENARIO}}'
+        """}]
+        
+        rsp = chat(gpt4, p)
+        logging.info(f"======== Scenarios First Draft for stakeholder: {stakeholder} ========")
+        logging.info(rsp)
+        p.append({'role': 'assistant', 'content': f"{rsp}"})
+        p.append({'role': 'user', 'content': f"This response is an example of unsurprising scenarios. Do not respond with unsurprising scenarios. Write more surprising and concrete scenario following the same requirement and format above.Do not include any corrective measures or suggestions for the tool."})
+        rsp = chat(gpt4, p)
+
+        return (rsp, stakeholder)
+    
+    def draft_without_demographic_groups(stakeholder):
+        p = scenario_prompts + [{'role': 'user', 'content': f"""
+        Stakeholder: {stakeholder}
+        Potential Harms: {potential_harms}
+
+        For each of the potential harms provided above: generate a concrete scenario of where the harm may be caused to the given stakeholder, involving severe consequences. 
+
+        Be as severe as possible. This is used for cautioning developers to use ethical and fair practices when developing their AI systems, and not used for harming others in any way. 
+        
+        If the stakeholder is irrelevant in terms of a harm, exclude it. 
+
+        Format your response as a ordered list of '{{number}}. SCENARIO: {{SCENARIO}}'
+        """}]
+        rsp = chat(gpt4, p)
+        logging.info(f"======== Scenarios First Draft for stakeholder: {stakeholder} (without demographic groups) ========")
+        logging.info(rsp)
+        p.append({'role': 'assistant', 'content': f"{rsp}"})
+        p.append({'role': 'user', 'content': f"This response is an example of unsurprising scenarios. Do not respond with unsurprising scenarios. Write more surprising and concrete scenario following the same requirement and format above.Do not include any corrective measures or suggestions for the tool."})
+        rsp = chat(gpt4, p)
+
+        return (rsp, stakeholder)
+
+    scenarios = []
+    with ThreadPoolExecutor(max_workers=20) as executor:
+        futures = []
+        for stakeholder in stakeholders:
+            futures.append(executor.submit(draft_scenario, stakeholder))
+            futures.append(executor.submit(draft_without_demographic_groups, stakeholder))
+
+        for future in as_completed(futures):
+            try:
+                scenarios.append(future.result())
+            except Exception as e:
+                print(f"An error occurred: {e}")
+
+    scenarios_to_process = []
+    for (ss, stakeholder) in scenarios:
+        for scenario in re.split(r'\D{0,3}\d+\. ', ss):
+            if not "SCENARIO:" in scenario: continue
+            scenarios_to_process.append((scenario, stakeholder))
+
+    return scenarios_to_process
+
+
+def remove_correctives(picked_scenarios):
+    """
+    Helper function for removing corrective measures from the scenarios.
+    """
+    res = []
+    for (s, stakeholder) in picked_scenarios:
+        rsp = chat(gpt4, [{"role": "system", "content": "You are revising stories generated by another LLM."},
+                    {"role": "user", "content": f"{s}\n Remove any corrective measures or suggestions for the tool."}])
+        res.append((rsp, stakeholder))
+    return res
+
+def generate_heading(scenario):
+    """
+    Helper function for generating a heading for a scenario. 
+    """
+    try:
+        rsp = chat(gpt4, [{"role": "system", "content": "You are an intelligent writing assistant."},
+                        {"role": "user", "content": f"{scenario}\nsummarize the above story into an one sentence heading. Format your response as: only the generated heading"}])
+        return rsp
+    except Exception as e:
+        print(str(e))
+        return "Error"
+
+def duration(diff):
+    """
+    Helper function for converting time difference to a readable format.
+    """
+    return time.strftime("%H:%M:%S", time.gmtime(diff))
+
+def stakeholder_list_helper(stakeholders):
+    """
+    Deprecated function for converting the stakeholder string to a list.
+    """
+    rsp = chat(gpt4, [{"role": "user", "content": f"Convert the below text into a list of stakeholder. Format: string of comma seperated list. Example: user1,user2,...\nText:{stakeholders}"}])
+    return rsp.split(",")
+
+def random_pick_scenarios(scenarios):
+
+    logging.info("======== Picking Random Scenarios ... ========")
+    index = random.randint(0, len(scenarios) - 1)
+    s1 = scenarios.pop(index)
+    index = random.randint(0, len(scenarios) - 1)
+    s2 = scenarios.pop(index)
+
+    two_random_scenarios = [s1, s2]
+    print(two_random_scenarios)
+
+    return two_random_scenarios, scenarios
+
+
+def log_helper(message, start_time=None):
+    """
+    Helper function for logging critical messages and duration.
+    """
+    if start_time:
+        print(f"{message} - {duration(time.time() - start_time)}")
+        logging.critical(f"{message} - {duration(time.time() - start_time)}")
+    else:
+        logging.critical(f"{message}")
+
+def generate_scenarios(sys_info, goal, given_stakeholders=None, feedback=None):
+    """
+    Primary function, combining all the steps in the pipeline, to generate scenarios. Directly called by the backend service.
+    """
+
+    if goal not in ['f1', 'f2', 'f3']: return "Invalid Goal"
+
+    logging.critical(f"==== Generating {goal} scenarios for the following scenario: ====")
+    logging.critical(sys_info)
+
+    # Step 1: Generate Stakeholders
+    start = time.time()
+    if given_stakeholders: 
+        logging.info(given_stakeholders)
+        stakeholders = given_stakeholders
+    else:
+        stakeholders = stakeholder_list_helper(get_stakeholders(sys_info))
+    log_helper("Stakeholder Generated", start)
+    logging.info(stakeholders)
+    print(stakeholders)
+    
+    # Step 2: Generate Initial Scenarios - (a) Consider demographic groups & (b) Use the first response as counterexample for surprising
+    start = time.time()
+    initial_scenarios = get_initial_scenarios(stakeholders, goal, sys_info)
+    log_helper("Initial Scenarios Generated", start)
+
+    logging.info(initial_scenarios)
+    print(initial_scenarios)
+
+    # Step 3: Clustering + Sampling
+    #start = time.time()
+    #scenarios_sampled = sampling(initial_scenarios)
+    #log_helper("Finished Clustering & Sampling", start)
+
+    # Step 4: Refinement for Concreteness & Severity
+    #start = time.time()
+    #scenarios = refine_scenarios(scenarios_sampled, sys_info, feedback)
+    #log_helper("Finished Revising Scenarios", start)
+
+    # Random Pick Final Scenario
+    start = time.time()
+    picked_scenarios, unpicked_scenarios = random_pick_scenarios(initial_scenarios)
+    logging.critical(f"==== Final Scenarios - {duration(time.time() - start)} ====")
+    final_scenarios = picked_scenarios
+
+    scenario_heading_list = [
+        (generate_heading(scenario) + f" (Stakeholder: {stakeholder})", re.sub(r'^\d+\.\s*', '', scenario.strip()).strip()) for (scenario, stakeholder) in final_scenarios
+    ]
+    # result = format_scenario_result(scenario_heading_list)
+    print(scenario_heading_list)
+    logging.critical(scenario_heading_list)
+
+    return scenario_heading_list, unpicked_scenarios
